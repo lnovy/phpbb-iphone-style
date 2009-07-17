39a3085870a069f986859b8541eac0b59edeed69
plawrence <plawrence@1c0c2b78-4311-11de-998b-8fedbbe774c7>
2009-07-17 19:47:41 +0000

    Temporary fix for Issue 3 (pagination links not opening in new pages)

--- trunk/template/iui/iui.js
+++ trunk/template/iui/iui.js
@@ -182,7 +182,9 @@ addEventListener("click", function(event)
     {
         function unselect() { link.removeAttribute("selected"); }
         
-        if (link.href && link.hash && link.hash != "#")
+        if (link.href && !link.target) {
+        	location.href = link.href;
+        } else if (link.href && link.hash && link.hash != "#")
         {
             link.setAttribute("selected", "true");
             iui.showPage($(link.hash.substr(1)));
